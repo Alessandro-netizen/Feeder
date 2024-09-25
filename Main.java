06266cea0a4a0407287068730d182cad73fa34c4
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-09-25 16:35:35 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,47 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Feeder f = new Feeder();
+        System.out.println(f);
+        Feeder g = new Feeder(500);
+        System.out.println(g.getCurrentFood());
+        g.simulateOneDay(12);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+  
+   
+    }
+}
+
+
+
+
+
+
+
+
+
