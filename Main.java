3d83bc67dd4b70e0add3d150508f5c2dcd511306
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-10-01 12:01:44 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,8 +10,36 @@ public class Main
 
 
 
-        feeder g = new Feeder(1000)
-        //System.out.println(g.getCurren)
+
+
+        Feeder g = new Feeder(1000);
+        System.out.println(g.getCurrentFood());
+        g.simulateOneDay(22);
+        System.out.println(g.toString());
+
+
+        Feeder h = new Feeder(100);
+        System.out.println(h.getCurrentFood());
+        h.simulateOneDay(5);
+        System.out.println(h.toString());
+
+
+        Feeder i = new Feeder(250);
+        System.out.println(i.getCurrentFood());
+        System.out.println(i.simulateManyDays(10, 5));
+
+
+        Feeder j = new Feeder(2400);
+        System.out.println(j.getCurrentFood());
+        System.out.println(j.simulateManyDays(10, 4));
+
+
+
+
+        Feeder k = new Feeder(0);
+        System.out.println(k.simulateManyDays(5, 10));
+
+
 
 
 
